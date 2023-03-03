ba3344a5baafd9c2c5e22a52feb576b2d276135e
kernel0x01 <80905310+kernel0x01@users.noreply.github.com>
2023-03-03 14:10:51 +0530

    Add files via upload

---
+++ binary.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    int b=0b0101;
+    printf("%d",b);
+}
---
+++ bitwise.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){               
+    int var=3; // 8 bit int (0000 0011)
+    printf("%d",var << 1); //(0000 0110)
+    return 0;
+}
